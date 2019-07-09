8777168eced50374c321cd226bb222929520d500
Giuseppe Petrosino <gius.petrosino@gmail.com>
2019-07-09 10:03:00 +0000

    Update tool.locations.groovy

--- modules/GLOEseq/tool.locations.groovy
+++ modules/GLOEseq/tool.locations.groovy
@@ -1,6 +1,6 @@
 // variables containing the location of the used tools
 TOOL_DEPENDENCIES="/fsimb/common/tools" // your local tools folder
-PROJECT_DEPENDENCIES=ESSENTIAL_PROJECT + "/NGSpipe2go/tools" // please copy the NGSpipe2Go tools to the project folder and include the location here
+PROJECT_DEPENDENCIES=ESSENTIAL_PROJECT + "/GLOEPipe/tools" // please copy the NGSpipe2Go tools to the project folder and include the location here
 TOOL_TRIMMOMATIC=TOOL_DEPENDENCIES + "/trimmomatic/0.36/"
 TOOL_GLOEseq=PROJECT_DEPENDENCIES+ "/GLOEscripts"
 TOOL_COLLECT=PROJECT_DEPENDENCIES + "/collectBpipeLogs"
