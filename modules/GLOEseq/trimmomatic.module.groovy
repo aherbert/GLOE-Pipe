1d2281b631113741e86a93ead7c165d2ac8eab32
Giuseppe Petrosino <gius.petrosino@gmail.com>
2019-07-11 13:30:03 +0000

    ESSENTIAL_TRIMMOMATIC variable

--- modules/GLOEseq/trimmomatic.module.groovy
+++ modules/GLOEseq/trimmomatic.module.groovy
@@ -5,16 +5,15 @@ Trimmomatic = {
 		author: "Giuseppe Petrosino"
 
 	output.dir   = TRIMREADS
-	def Trimmomatic_FLAGS = "SE -phred33 " +
-                            Trimmomatic_THREADS + " "
-                            
-        def Trimmomatic_OPTIONS = "ILLUMINACLIP:/fsimb/common/tools/trimmomatic/0.36/adapters/TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:4:15 MINLEN:36"
-	
+	def Trimmomatic_FLAG1 = "SE -phred33 " +
+                            Trimmomatic_THREADS 
+    def Trimmomatic_FLAG2 = "ILLUMINACLIP:" + Trimmomatic_ADAPTER + ":2:30:10 SLIDINGWINDOW:4:15 MINLEN:36"
+    
 	transform(".fastq.gz") to ("_trimmed.fastq.gz") {
 		exec """
 			module load trimmomatic/${TRIMMOMATIC_VERSION} &&
 
-                        java -jar ${TOOL_TRIMMOMATIC}/trimmomatic-0.36.jar $Trimmomatic_FLAGS $input $output $Trimmomatic_OPTIONS	
+                        java -jar ${TOOL_TRIMMOMATIC}/trimmomatic-0.36.jar $Trimmomatic_FLAG1 $input $output $Trimmomatic_FLAG2	
 		""","Trimmomatic"
 	}
 
