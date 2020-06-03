fbeddf64359ccebc877fb164485749a83afa6fe2
Giuseppe Petrosino <gius.petrosino@gmail.com>
2020-06-03 15:04:03 +0000

    ESSENTIAL_TSS variable

--- modules/GLOEseq/breaks_annotation.vars.groovy
+++ modules/GLOEseq/breaks_annotation.vars.groovy
@@ -2,7 +2,7 @@ breaks_annotation_FILES="breakData="+ RESULTS+ "/macs2"
 breaks_annotation_TRANSCRIPT_TYPE="transcriptType=Bioconductor"
 breaks_annotation_TRANSCRIPT_DB="transcriptDb=" + ESSENTIAL_TXDB
 breaks_annotation_ORGDB="orgDb=" + ESSENTIAL_ANNODB   
-breaks_annotation_REGIONTSS="regionTSS=200"
+breaks_annotation_REGIONTSS="regionTSS=" + ESSENTIAL_TSS
 breaks_annotation_OUTDIR="out="+ RESULTS+ "/breaks_annotation"
 breaks_annotation_INDEX="index=" + ESSENTIAL_BOWTIE_GENOME_INDEX
 breaks_annotation_EXTRA=""
