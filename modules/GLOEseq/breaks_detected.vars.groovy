1a3e2b47369bd351249417f042a635efe1669c4f
Giuseppe Petrosino <gius.petrosino@gmail.com>
2020-06-08 09:51:59 +0000

    Rename "RE" into "EB"

--- modules/GLOEseq/breaks_detected.vars.groovy
+++ modules/GLOEseq/breaks_detected.vars.groovy
@@ -1,6 +1,6 @@
 breaks_detected_FILES="breakData="+ RESULTS+ "/macs2"
-breaks_detected_RE1="RestrictionEnzyme1=" + ESSENTIAL_RE1
-breaks_detected_RE2="RestrictionEnzyme2=" + ESSENTIAL_RE2
-breaks_detected_RE3="RestrictionEnzyme3=" + ESSENTIAL_RE3
+breaks_detected_EB1="ExpectedBreaks1=" + ESSENTIAL_EB1
+breaks_detected_EB2="ExpectedBreaks2=" + ESSENTIAL_EB2
+breaks_detected_EB3="ExpectedBreaks3=" + ESSENTIAL_EB3
 breaks_detected_EXTRA=""
 breaks_detected_OUTDIR="out="+ RESULTS+ "/macs2"
