48ea263552450d01f8ce9675c4b0f005a2506f02
Giuseppe Petrosino <gius.petrosino@gmail.com>
2023-01-18 10:31:55 +0000

    Cutadapt module for paired-end data

---
+++ modules/GLOEseq/cutadapt_pe.module.groovy
@@ -0,0 +1,45 @@
+Cutadapt_pe = {
+	doc title: "Cutadapt module for paired-end data",
+		desc:  "A flexible read trimming tool for Illumina NGS data",
+		bpipe_version: "tested with bpipe 0.9.9.8.slurm",
+		author: "Giuseppe Petrosino, Anke Busch"
+
+	output.dir   = TRIMREADS
+
+	// create the log folder if it doesn't exists
+    def TRIM_LOGS = new File(LOGS + "/trimAdapter")
+    if (!TRIM_LOGS.exists()) {
+            TRIM_LOGS.mkdirs()
+    }
+
+
+    def SAMPLENAME = new File(input.prefix.prefix)
+    def SAMPLENAME_BASE = SAMPLENAME.getName()
+    def SAMPLENAME_BASE_PRUNED = SAMPLENAME_BASE.replace(".R1", "") // delete .R1 in combined log file of pe design
+
+
+	// Cutadapt options
+	def CUTADAPT_FLAGS = "-a file:" + CUTADAPT_ADAPTER + 
+                         " -A file:" + CUTADAPT_ADAPTER + 
+                             " --overlap=" + CUTADAPT_OVERLAP + 
+                             " --minimum-length=" + CUTADAPT_MINREADLEN +
+                             " --error-rate=" + CUTADAPT_ERROR_RATE + 
+                             " --nextseq-trim=" + CUTADAPT_NEXTSEQTRIM +
+                             " " + CUTADAPT_EXTRA
+
+	transform("*.fastq.gz") to (".trimmed.fastq.gz") {
+		exec """
+
+            module load cutadapt/${CUTADAPT_VERSION} &&
+
+                        if [ -n "\$SLURM_JOBID" ]; then
+                                export TMPDIR=/jobdir/\${SLURM_JOBID};
+                        fi &&
+
+                        cutadapt $CUTADAPT_THREADS $CUTADAPT_FLAGS --output=\${TMPDIR}/${SAMPLENAME_BASE_PRUNED}.R1.trimmed.fastq.gz --paired-output \${TMPDIR}/${SAMPLENAME_BASE_PRUNED}.R2.trimmed.fastq.gz $input1 $input2 1> ${TRIM_LOGS}/${SAMPLENAME_BASE_PRUNED}.cutadapt.log &&
+                        mv -t $output.dir \${TMPDIR}/${SAMPLENAME_BASE_PRUNED}*.trimmed.fastq.gz 
+
+		""","Cutadapt_pe"
+	}
+
+}
