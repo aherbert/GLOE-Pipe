3dadf4951e41e7392262d146aa19c2c8dc8384b5
Giuseppe Petrosino <gius.petrosino@gmail.com>
2023-01-18 10:46:56 +0000

    Update bed2bz2.module.groovy

--- modules/GLOEseq/bed2bz2.module.groovy
+++ modules/GLOEseq/bed2bz2.module.groovy
@@ -2,7 +2,7 @@ bed2bz2 = {
 	doc title: "bed2bz2",
 		desc:  "Compress Bed file",
 		constraints: "none.",
-		bpipe_version: "tested with bpipe 0.9.9.3.slurm",
+		bpipe_version: "tested with bpipe 0.9.9.5.slurm",
 		author: "Giuseppe Petrosino"
 
 	output.dir=BED
