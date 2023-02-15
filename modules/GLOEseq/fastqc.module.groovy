287f6075f4bcc0c466463994d0be5c9f48dc31de
Giuseppe Petrosino <gius.petrosino@gmail.com>
2023-02-15 17:06:00 +0000

    Update fastqc.module.groovy

--- modules/GLOEseq/fastqc.module.groovy
+++ modules/GLOEseq/fastqc.module.groovy
@@ -3,10 +3,11 @@ FastQC = {
 		desc:  "Quality control of input file",
 		constraints: "Only supports compressed FASTQ files",
 		bpipe_version: "tested with bpipe 0.9.9.5.slurm",
-		author: "Giuseppe Petrosino"
-    
-	output.dir   = FASTQC_OUTDIR
-	def FASTQC_FLAGS = "--extract --quiet"
+		author: "Giuseppe Petrosino, Anke Busch"
+        
+        var subdir : ""
+        output.dir = FASTQC_OUTDIR + "/$subdir"     
+	def FASTQC_FLAGS = "--extract --quiet" + FASTQC_THREADS
 	
 	transform("*.fastq.gz") to ("_fastqc.zip") {
 		exec """
