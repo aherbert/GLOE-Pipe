5ce4a3a273711d3cef661e2be9c66f87dbd1b4f3
Giuseppe Petrosino <gius.petrosino@gmail.com>
2023-04-04 14:26:22 +0000

    Update fastqc.vars.groovy

--- modules/GLOEseq/fastqc.vars.groovy
+++ modules/GLOEseq/fastqc.vars.groovy
@@ -1,3 +1,3 @@
 //vars for task FastQC from catalog NGS, version 1
 FASTQC_OUTDIR=QC + "/fastqc"
-FASTQC_THREADS=Integer.toString(ESSENTIAL_THREADS)
+FASTQC_THREADS=" -t " + Integer.toString(ESSENTIAL_THREADS)
