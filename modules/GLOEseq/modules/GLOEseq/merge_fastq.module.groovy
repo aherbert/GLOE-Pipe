1ef4600db56c1db087f9b34534146fd072bfb0a5
Giuseppe Petrosino <gius.petrosino@gmail.com>
2023-04-18 13:17:36 +0000

    New merge_fastq.module.groovy file

---
+++ modules/GLOEseq/modules/GLOEseq/merge_fastq.module.groovy
@@ -0,0 +1,33 @@
+Merge_fastq = {
+    doc title: "Merge Fastq files",
+        desc:  "adds index to the R1 fastq",
+        constraints: "",
+        bpipe_version: "tested with bpipe 0.9.9.3.slurm",
+        author: "Giuseppe Petrosino"
+
+    output.dir = MERGEDREADS
+
+
+    // extract the base of the input file name (w/o the directory, w/o fastq.gz)
+       def OUTPUTFILE = input1
+       int path_index = OUTPUTFILE.lastIndexOf("/")
+       OUTPUTFILE = OUTPUTFILE.substring(path_index+1)
+       OUTPUTFILE = (OUTPUTFILE =~ /.R1.fastq.gz/).replaceFirst("")
+
+
+    produce(OUTPUTFILE + "*.merged.fastq.gz") {
+		exec """
+
+                        if [ -n "\$SLURM_JOBID" ]; then
+                                export TMPDIR=/jobdir/\${SLURM_JOBID};
+                        fi &&
+
+
+
+               paste -d "" <(zcat $input2) <(zcat $input1 | sed 's/^+.*//' | sed 's/^@.*//') | gzip > ${MERGEDREADS}/${OUTPUTFILE}.R1.merged.fastq.gz &&
+               cp $input3 ${MERGEDREADS}/${OUTPUTFILE}.R3.merged.fastq.gz
+
+        ""","Merge_fastq"
+    }
+
+}
